77243cac56c70fc36466d92dddfeb4a71737b54f
Kolchanov.VI <Kolchanov.VI@parma.ru>
2021-09-16 12:46:33 +0500

    11. Bean scope

--- src/main/java/com/udemy/udemyspring/spring_introduction/Dog.java
+++ src/main/java/com/udemy/udemyspring/spring_introduction/Dog.java
@@ -2,6 +2,10 @@ package com.udemy.udemyspring.spring_introduction;
 
 public class Dog implements Pet {
 
+    public Dog() {
+        System.out.println("Dog is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Wow-wow");
---
+++ src/test/java/com/udemy/udemyspring/Test4.java
@@ -0,0 +1,43 @@
+package com.udemy.udemyspring;
+
+import com.udemy.udemyspring.spring_introduction.Cat;
+import com.udemy.udemyspring.spring_introduction.Dog;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class Test4 {
+
+    @Test
+    public void testSingleton() {
+        // Можно перечислять сколь угодно много xml файлов через запятую
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext2.xml"
+        );
+
+        Dog myDog = context.getBean("myDog", Dog.class);
+        Dog yourDog = context.getBean("myDog", Dog.class);
+
+        assertThat(myDog, is(yourDog));
+
+        context.close();
+    }
+
+    @Test
+    public void testPrototype() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext2.xml"
+        );
+
+        Cat myCat = context.getBean("myCat", Cat.class);
+        Cat yourCat = context.getBean("myCat", Cat.class);
+
+        assertThat(myCat, is(not(yourCat)));
+
+        context.close();
+    }
+
+}
